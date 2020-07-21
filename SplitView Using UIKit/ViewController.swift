346349ede1bb69aa7df3e33b8b2c168721d91aad
MannaICT13 <khmannaict13@gmail.com>
2020-07-22 04:06:16 +0600

    Initial Commit

---
+++ SplitView Using UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitView Using UIKit
+//
+//  Created by Md Khaled Hasan Manna on 22/7/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
